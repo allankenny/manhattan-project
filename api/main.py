b2619b501c4e6f4a756caff460236636dbdc9f54
allankenny <allan.kennedy@gmail.com>
2025-06-23 14:36:51 -0300

    middleware

--- api/main.py
+++ api/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.concurrency import asynccontextmanager
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.db import db
 from src.handlers.executions import router as executions_router
@@ -14,4 +15,12 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(executions_router)
